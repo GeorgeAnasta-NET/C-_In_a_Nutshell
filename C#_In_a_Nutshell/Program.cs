ff4701f2e684280f59417470faebd59f63cd668f
Georgios Anastasiadis <Georgios.Anastasiadis@arhs-dev-hellas.com>
2024-05-28 14:19:37 +0300

    Add project files.

---
+++ C#_In_a_Nutshell/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__In_a_Nutshell
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FeetToInches(30));
+            Console.WriteLine(FeetToInches(100));
+        }
+
+        static int FeetToInches(int feet)
+        {
+            int inches = feet * 12;
+            return inches;
+        }
+    }
+}

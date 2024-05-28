f7539b6f58a87c72c86d03547419008b4ef71503
Georgios Anastasiadis <Georgios.Anastasiadis@arhs-dev-hellas.com>
2024-05-28 15:20:39 +0300

    Predefined Types

--- C#_In_a_Nutshell/Program.cs
+++ C#_In_a_Nutshell/Program.cs
@@ -11,8 +11,32 @@ namespace C__In_a_Nutshell
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(FeetToInches(30));
-            Console.WriteLine(FeetToInches(100));
+            //Console.WriteLine(FeetToInches(30));
+            //Console.WriteLine(FeetToInches(100));
+
+            string message = "Hello World";
+            string  upperMessage = message.ToUpper();
+            Console.WriteLine(upperMessage);                //HELLO WORLD
+
+            int x = 2015;
+            message = message + x.ToString();
+            Console.WriteLine(message);                     //Hello World2015
+
+            bool simpleVar =  false;
+            if (simpleVar)
+            {
+                Console.WriteLine("This will not print");
+            }
+
+            int y = 5000;
+            bool lessThanAMile = y < 5280;
+            if(lessThanAMile)
+            {
+                Console.WriteLine("this will print");
+            }
+
+            Console.Read();
+
         }
 
         static int FeetToInches(int feet)
